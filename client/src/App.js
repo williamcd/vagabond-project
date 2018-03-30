71c3367bc2d610ca49164a0f721bc1d4cc5ed51f
owenliversidge <owen.liversidge@gmail.com>
2018-03-30 15:38:25 -0400

    index page basic styling

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Footer from "./Footer";
 
 injectGlobal`
 @import url('https://fonts.googleapis.com/css?family=Sarina');
+@import url('https://fonts.googleapis.com/css?family=Diplomata+SC');
 `;
 
 class App extends Component {
@@ -16,6 +17,11 @@ class App extends Component {
         <Header />
         <PageWrapper>
           <h1>Atlanta</h1>
+          <img src="https://i.imgur.com/42zYDsw.jpg" />
+          <h1>London</h1>
+          <img src="https://i.imgur.com/6X26EdW.jpg" />
+          <h1>San Francisco</h1>
+          <img src="https://i.imgur.com/6YH983R.jpg" />
         </PageWrapper>
         <Footer />
       </div>
@@ -29,11 +35,29 @@ const PageWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  background-image: url("https://i.imgur.com/WhBsOjy.jpg");
+  background-image: url("https://i.imgur.com/mZVpFqq.jpg");
   background-repeat: no-repeat;
   background-position: center center;
   background-attachment: fixed;
   background-size: cover;
+  align-items: center;
+
+  img {
+    width: 60%;
+    margin: 15px;
+    border-radius: 15px;
+    padding: 15px;
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2),
+      0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+
+  h1 {
+    font-family: 'Diplomata SC', cursive;
+    color: white;
+    font-size: 28px; 
+    text-shadow: 1px 2px black;
+    margin: 28px 0px;
+  }
 `;
 
 export default App;
