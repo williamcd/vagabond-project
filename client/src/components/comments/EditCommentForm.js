f69fb4ebc8cfce43fd8457f89509313b67a2d127
owenliversidge <owen.liversidge@gmail.com>
2018-04-03 10:41:53 -0400

    cleaned up routes

--- client/src/components/comments/EditCommentForm.js
+++ client/src/components/comments/EditCommentForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import DeleteConfirm from "./DeleteConfirm";
 import axios from 'axios'
 
 class EditCommentForm extends Component {
